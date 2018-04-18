2833a5f58c4b35cc451bbb3fff2abed263b57e5e
Cristobal Belda-Perez <cristobal.belda-perez@canon-europe.com>
2018-04-18 13:58:02 +0200

    added basic instructions

--- setup.sh
+++ setup.sh
@@ -1,42 +1,19 @@
 #!/bin/bash
-RED="\033[0;31m"
-GREEN="\033[0;32m"
-NC="\033[0m"
-NODE_VERSION=v8.9.4
-NPM_VERSION=5.6.0
-if [ "$(node -v)" == "$NODE_VERSION" ];
-    then
-    if [ "$(npm -v)" == "$NPM_VERSION" ];
-    then 
-        if [ "$(code -v)" ];
-            then
-            echo -e "${GREEN}SUCCESS >> set up Angular project...${NC}"
-            npm i -g @angular/cli
-            ng version
-            ng new angular-material-example --prefix example --style scss
-            cd angular-material-example
-            npm i -S @angular/material @angular/cdk @angular/animations
-            git clone https://github.com/oasp/oasp-vscode-ide.git
-            ./oasp-vscode-ide/install-extensions.sh
-            rm -rf oasp-vscode-ide
-            code .
-            ng serve -o
-        else
-            echo -e "${RED}ERROR >> Visual Studio Code not installed...${NC}"   
-        fi
-    else
-        echo -e "${RED}ERROR: Node Package Manager (npm) not installed or not installed the LTS version...${NC}"
-        echo "Installing npm ${NPM_VERSION} ..."
-        npm i -g npm@5.6.0
-        echo "----------------------------"
-        echo "PLEASE RUN THIS SCRIPT AGAIN"
-        echo "----------------------------"
-        read -n 1 -s -r -p "Press any key to exit..."
-    fi
-else
-    echo -e "${RED}ERROR >> Node.js not installed or not installed the LTS version...${NC}"
-    echo "------------------------------------------------------------------------------------------------"
-    echo "PLEASE INSTALL THE CORRECT Node.js VERSION FROM https://nodejs.org/en/ AND RUN THIS SCRIPT AGAIN"
-    echo "------------------------------------------------------------------------------------------------"
-    read -n 1 -s -r -p "Press any key to exit..."
-fi
+
+# 1. create Angular project via Angular CLI
+ng new angular-material-example --prefix example --style scss
+
+# 2. once created, go to the project's directory
+cd angular-material-example
+
+# 3. install npm packages to get the Material Design look on the app
+npm i -S @angular/material @angular/cdk @angular/animations
+
+# 4. (optional) configure Visual Studio Code with cool plug-ins
+# git clone https://github.com/oasp/oasp-vscode-ide.git
+# ./oasp-vscode-ide/install-extensions.sh
+# rm -rf oasp-vscode-ide
+
+# 5. open VSCode with the code and Chrome with the app
+code .
+ng serve -o
