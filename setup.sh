970528d4e18c8c0dcba58c33bae8a0d582b4e67f
cbelda <cbeldaperez@gmail.com>
2018-04-19 09:25:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # 1. create Angular project via Angular CLI
-ng new angular-material-example --prefix example --style scss
+ng new ng-material-starter --prefix example --style scss
 
 # 2. once created, go to the project's directory
-cd angular-material-example
+cd ng-material-starter
 
 # 3. install npm packages to get the Material Design look on the app
-npm i -S @angular/material @angular/cdk @angular/animations
+yarn add @angular/material @angular/cdk @angular/animations
 
 # 4. (optional) configure Visual Studio Code with cool plug-ins
 # git clone https://github.com/oasp/oasp-vscode-ide.git
